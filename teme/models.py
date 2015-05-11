4d732b23cb40496e7bcd6bc8f5192892756126f2
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-12 01:19:40 +0300

    Connect some shit around

--- teme/models.py
+++ teme/models.py
@@ -1,6 +1,8 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+from .constants import MIN_RATING_STAR_VALUE, MAX_RATING_STAR_VALUE
+
 
 class Teacher(models.Model):
     name = models.CharField(max_length=200)
@@ -10,8 +12,20 @@ class Course(models.Model):
     name = models.CharField(max_length=200)
     teacher = models.ForeignKey(Teacher)
 
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Material(models.Model):
+    course = models.ForeignKey(Course)
+    name = models.CharField(max_length=200)
+
 
 class Rating(models.Model):
-    grade = models.IntegerField(validators=[MinValueValidator(1),
-                                            MaxValueValidator(5)])
-    course = models.ForeignKey(Course)
+    grade = models.IntegerField(validators=[
+        MinValueValidator(MIN_RATING_STAR_VALUE),
+        MaxValueValidator(MAX_RATING_STAR_VALUE)])
+    course = models.ForeignKey(Material)
--- teme/views.py
+++ teme/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Material, Teacher
+
 
 def index(request):
-    return render(request, 'teme/index.html', {})
+    teachers = []
+    for teacher in Teacher.objects.all():
+        courses = [str(x) for x in teacher.course_set.all()]
+        teacher_context = {
+            'name': teacher.name,
+            'courses': ', '.join(courses),
+        }
+        teachers.append(teacher_context)
+
+    return render(request, 'teme/index.html', {
+        'materials': Material.objects.all(),
+        'teachers': teachers,
+    })
