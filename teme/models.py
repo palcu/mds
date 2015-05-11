f1ce5ab7728bc9a6f9ee9595af741f0d3d03d21d
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-11 23:16:53 +0300

    Add models #1

--- teme/models.py
+++ teme/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    teacher = models.ForeignKey(Teacher)
+
+
+class Rating(models.Model):
+    grade = models.IntegerField(validators=[MinValueValidator(1),
+                                            MaxValueValidator(5)])
+    course = models.ForeignKey(Course)
