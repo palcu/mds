2df254f87aba26c7492bf01916f59ce457de033c
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-22 16:04:59 +0300

    Implement get rating for teacher #2

--- teme/views.py
+++ teme/views.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -6,13 +8,20 @@ from .models import Course, Teacher
 
 def index(request):
     teachers = []
-    for teacher in Teacher.objects.all():
-        courses = [str(x) for x in teacher.course_set.all()]
+    for teacher in Teacher.objects.all()[:10]:
+        courses = []
+        ratings = []
+        for course in teacher.course_set.all():
+            courses.append(str(course))
+            ratings += [x.grade for x in course.rating_set.all()]
+
         teacher_context = {
             'name': teacher.name,
             'courses': ', '.join(courses),
+            'rating': int(mean(ratings)) if len(ratings) else 3
         }
         teachers.append(teacher_context)
+    print(teachers)
 
     return render(request, 'teme/index.html', {
         'courses': Course.objects.all(),
