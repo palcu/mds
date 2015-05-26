eecc62bb52d6ad860237c643965a23d1682161a6
Alex Palcuie <alex.palcuie@hootsuite.com>
2015-05-26 09:42:20 +0300

    Add tests

--- teme/tests.py
+++ teme/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.test import Client
+from django.test.utils import setup_test_environment
 
-# Create your tests here.
+
+class TemeTests(TestCase):
+    def test_home(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_box(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/')
+        self.assertTrue('Cauta' in str(response.content))
+
+    def test_courses(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/courses')
+        self.assertEqual(response.status_code, 200)
+
+    def test_teachers(self):
+        setup_test_environment()
+        client = Client()
+        response = client.get('/teachers')
+        self.assertEqual(response.status_code, 200)
